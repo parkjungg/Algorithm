59a182b39770a804fb5df9823378eb0bd91eb414
pjk3665@naver.com <pjk3665@naver.com>
2025-03-29 18:48:37 +0900

    실버 3문제 추가

---
+++ src/Silver/Num2346.java
@@ -0,0 +1,40 @@
+package Silver;
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+// Silver 3 풍선 터뜨리기
+public class Num2346 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Deque<int[]> deque = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            deque.add(new int[]{i, sc.nextInt()});
+        }
+        int num = 0;
+
+        while (!deque.isEmpty()) {
+            if(num > 0) {
+                for (int i = 0; i < num - 1; i++) {
+                    deque.add(deque.poll());
+                }
+            } else {
+                for (int i = 0; i < -num; i++) {
+                    deque.addFirst(deque.pollLast());
+                }
+            }
+
+            int[] current = deque.pollFirst();
+            sb.append(current[0]).append(" ");
+            num = current[1];
+        }
+        System.out.println(sb);
+    }
+}
